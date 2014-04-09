d46bbecd40f1fbf8e37064ca324dd8adaadcffa7
Seiya Nuta <nuta@seiya.me>
2014-04-09 23:41:27 +0900

    reg_all_in_standard_timetable_for2.pyを追加

--- README.md
+++ README.md
@@ -32,6 +32,9 @@ $ univ help
 用例
 -----
 ```
+$ univ reg GB31801  # オペレーティングシステムIIを履修
+$ univ unreg GB10214  # 1,2クラス向け線形代数IIを切る
+$ ./tools/reg_all_in_standard_timetable_for2.py|xargs -n1 ./univ reg  # 二年生の時間割で共通するものを全て履修登録
 $ univ ach|awk '$1 == "A+"{ print }'   # A+とった授業
 $ univ achsum|awk '$1 == "GPA:"{ print $2 }'|mail -s "my GPA" mom@example.com  # 母親にGPAを教える
 ```
---
+++ tools/reg_all_in_standard_timetable_for2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import sys
+import requests
+from pyquery import PyQuery as pq
+
+sys.stderr.write("""\
+***************
+
+クラス別の授業、体育や総合科目などは自分で
+登録しましょう。そこまで登録してくれるのは
+AIが完成してからです。
+
+***************
+""")
+
+d = pq(url="http://www.coins.tsukuba.ac.jp/syllabus/timetable2.html")
+for a in d("a").items():
+    if "_" in a.attr["href"] or not a.attr["href"].startswith("G"):
+        continue
+    print(a.attr["href"][0:7])
