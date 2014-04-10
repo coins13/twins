083b4056ea83787946d63fa7017fd6b9f6bae5ea
Seiya Nuta <nuta@seiya.me>
2014-04-10 16:33:33 +0900

    落とし穴回避

--- tools/reg_all_in_standard_timetable_for2.py
+++ tools/reg_all_in_standard_timetable_for2.py
@@ -15,6 +15,7 @@ AIが完成してからです。
 
 d = pq(url="http://www.coins.tsukuba.ac.jp/syllabus/timetable2.html")
 for a in d("a").items():
-    if "_" in a.attr["href"] or not a.attr["href"].startswith("G"):
+    if "_" in a.attr["href"] or not a.attr["href"].startswith("G") or \
+       a.attr["href"] == "GB11926":
         continue
     print(a.attr["href"][0:7])
