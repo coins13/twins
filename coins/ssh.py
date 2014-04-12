c9f3dfa28867fbf73cc8aeb729e1f816639605b0
Seiya Nuta <nuta@seiya.me>
2014-04-12 19:03:53 +0900

    ssh: ~/.ssh/known_hostsにあるホストから接続を試みる

--- coins/ssh.py
+++ coins/ssh.py
@@ -1,3 +1,4 @@
+import os
 import random
 import socket
 
@@ -15,6 +16,16 @@ def shake_hands (host):
 
 def search_for_ssh_server ():
     """ 生きてるiMacを探す """
+
+    # ~/.ssh/known_hostsにあるやつから試す
+    try:
+        with open(os.path.expanduser("~/.ssh/known_hosts")) as f:
+            for l in f.readlines():
+                host = l.split(",")[0]
+                if host.endswith("coins.tsukuba.ac.jp") and shake_hands(host):
+                    return host
+    except IndexError:
+        pass
 
     hosts = ["borage%02d" % a for a in range(1, 50) ]
 
