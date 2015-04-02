9dc91fbc2b2151c4423166423e31ce1605aa7feb
Seiya Nuta <nuta@seiya.me>
2015-04-02 15:44:56 +0900

    まともなバージョニング

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="twins",
-    version="101.0",
+    version="1.0.0",
     description="Command-line interface to Univ. of Tsukuba's" +
                 "course registration system",
     long_description="",
